d483076eb800ed0fd4d9c2d9699aae6f68781895
priya kumari <priya.kumari_cs19@gla.ac.in>
2020-08-30 16:06:31 +0530

    create the objects of book for FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,6 +6,12 @@
  * */
 package execution;
 
+import definitions.Book;
+import definitions.Library;
+import definitions.Student;
+
+import java.util.Scanner;
+
 public class FrontDesk {
     public static final int EXIT = 4;
     private static final int ISSUE_A_BOOK = 1;
@@ -13,6 +19,27 @@ public class FrontDesk {
     private static final int SHOW_ALL_BOOKS_ISSUED = 3;
 
     public static void main(String[] args) {
+        Scanner scannerObject = new Scanner(System.in);
+//OBJECT CREATED FOR BOOK
+        Book bookobject = new Book();
+        Library libraryObject = new Library();
+        Student studentObject = new Student();
+        System.out.println(" ENTER YOUR FIRST NAME");
+        String firstname = scannerObject.nextLine();
+        System.out.println("ENTER YOUR MIDDLE NAME:");
+        String middlename = scannerObject.nextLine();
+        System.out.println("ENTER YOUR LAST NAME:");
+        String lastname = scannerObject.nextLine();
+        System.out.println("ENTER YOUR ROLL NUMBER:");
+        Long rollNumber = scannerObject.nextLong();
+        scannerObject.nextLine();
+        System.out.println("-=-=--=-=-\"WELCOME," + firstname + ",TO THE FRONT DESK\"-=-=--=-=-");
+        System.out.println("HOW MAY I HELP YOU TODAY?");
+        System.out.println("1. Issue a new book for me.");
+        System.out.println("2. Return a previously issues book for me.");
+        System.out.println("3. Show me all my issues books.");
+        System.out.println("4. Exit.");
+        System.out.println("ENTER YOUR CHOICE FROM: [1,2,3,4]");
 
     }
 }
