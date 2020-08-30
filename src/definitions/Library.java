6c660cf6efd02ce0d959252667dff30215d21f4c
priya kumari <priya.kumari_cs19@gla.ac.in>
2020-08-30 10:26:15 +0530

    create constructor for Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -9,4 +9,23 @@ package definitions;
 public class Library {
     private Book[] booksCurrentlyAvailable;
 
+    //created the constructor
+    public Library() {
+        this.booksCurrentlyAvailable = new Book[20];
+        //        for (int i = 0; i < Book.length(); i++) {
+        //
+        //     }
+        this.booksCurrentlyAvailable[0] = new Book("English", "R.K Narayan", "5457982872581");
+        this.booksCurrentlyAvailable[1] = new Book("DBMS", "Ramez Elmasri", "228522265256639");
+        this.booksCurrentlyAvailable[2] = new Book("OOPs", "Grady Booch" + "", "56257029460839");
+    }
+
+    public Book[] getBooksCurrentlyAvailable() {
+        return booksCurrentlyAvailable.clone();
+    }
+
+    public void setBooksCurrentlyAvailable(Book[] booksCurrentlyAvailable) {
+        this.booksCurrentlyAvailable = booksCurrentlyAvailable;
+    }
+
 }
