bc2adb5734b0f1a2f5f414fb67645055b992a8ff
priya kumari <priya.kumari_cs19@gla.ac.in>
2020-08-30 13:14:01 +0530

    create getter setter method of field for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -23,4 +23,49 @@ public class Student {
         this.studentUniversityRollNo = studentUniversityRollNo;
         this.numberOfBooksIssued = numberOfBooksIssued;
     }
+
+    public Student() {
+        this.BooksIssuedByStudent = new Book[20];
+    }
+
+    public String getFirstNameOfStudent() {
+        return firstNameOfStudent;
+    }
+
+    public void setFirstNameOfStudent(String firstNameOfStudent) {
+        this.firstNameOfStudent = firstNameOfStudent;
+    }
+
+    public String getMiddleNameOfStudent() {
+        return middleNameOfStudent;
+    }
+
+    public void setMiddleNameOfStudent(String middleNameOfStudent) {
+        this.middleNameOfStudent = middleNameOfStudent;
+    }
+
+    public String getLastNameOfStudent() {
+        return lastNameOfStudent;
+    }
+
+    public void setLastNameOfStudent(String lastNameOfStudent) {
+        this.lastNameOfStudent = lastNameOfStudent;
+    }
+
+    public long getStudentUniversityRollNo() {
+        return getStudentUniversityRollNo();
+    }
+
+    public void setStudentUniversityRollNo(long studentUniversityRollNo) {
+        this.studentUniversityRollNo = studentUniversityRollNo;
+    }
+
+    public Book[] getBooksIssuedByStudent() {
+        return BooksIssuedByStudent.clone();
+    }
+
+    public void setNumberOfBooksIssued(int numberOfBooksIssued) {
+        this.numberOfBooksIssued = numberOfBooksIssued;
+    }
+
 }
