580f4d349432301d540223b8f12ec153640c0434
priya kumari <priya.kumari_cs19@gla.ac.in>
2020-08-30 12:39:22 +0530

    add user details

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,10 +1,9 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
+ *  User: PRIYA KUMARI (priya652)
+ *  Date: 30/08/20
  *  Time: 3:49 PM
  *  File Name : Student.java
  * */
 package definitions;
-
 public class Student {
 }
